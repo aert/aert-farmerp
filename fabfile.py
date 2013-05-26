431641cfbef9250d82573d6948e2b33a2d076ced
aert <dev.aert@gmail.com>
2013-05-26 19:56:48 +0200

    Added runserver(local)

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,8 @@
 from fabric.api import *
+
+@task
+def runserver(env="local"):
+    settings='aert_bookkeeping.settings.{}'.format(env)
+    local('django-admin.py runserver --settings={}'\
+          .format(settings))
+
