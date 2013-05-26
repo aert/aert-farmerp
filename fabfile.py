a9ab2e8f3210dffb388cbfb6abec44303091ab2d
aert <dev.aert@gmail.com>
2013-05-27 00:35:40 +0200

    Added: SyncDB

--- fabfile.py
+++ fabfile.py
@@ -6,3 +6,9 @@ def runserver(env="local"):
     local('django-admin.py runserver --settings={}'\
           .format(settings))
 
+@task
+def syncdb(env="local"):
+    settings='aert_bookkeeping.settings.{}'.format(env)
+    local('django-admin.py syncdb --settings={}'\
+          .format(settings))
+
