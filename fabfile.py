de5f082f83969065807b2653fe06761ae63956aa
aert <dev.aert@gmail.com>
2013-05-31 23:13:46 +0200

    modified: fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,16 @@
 from fabric.api import *
 
+SETTINGS_FORMAT = 'aert_bookkeeping_site.settings.{}'
+
 @task
 def runserver(env="local"):
-    settings='aert_bookkeeping.settings.{}'.format(env)
+    settings=SETTINGS_FORMAT.format(env)
     local('django-admin.py runserver --settings={}'\
           .format(settings))
 
 @task
 def syncdb(env="local"):
-    settings='aert_bookkeeping.settings.{}'.format(env)
+    settings=SETTINGS_FORMAT.format(env)
     local('django-admin.py syncdb --settings={}'\
           .format(settings))
 
