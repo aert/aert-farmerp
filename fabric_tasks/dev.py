b1c21e817a0188705449f9ff3d5ab4ca3caaa3cc
aert <dev.aert@gmail.com>
2013-06-15 20:12:29 +0200

    #20: reformating

--- fabric_tasks/dev.py
+++ fabric_tasks/dev.py
@@ -1,12 +1,12 @@
+from os import path
+import hashlib
+
 from fabric.api import *
 from fabric import colors
-from fabtools.python import virtualenv
-from os import path
 
-import os, hashlib
 
 # Local paths
-LOCAL_ROOT = path.dirname(path.dirname(os.path.realpath(__file__)))
+LOCAL_ROOT = path.dirname(path.dirname(path.realpath(__file__)))
 VENV_PATH = "/home/ari/Dev/Envs/aert-bookkeeping"
 
 
@@ -20,7 +20,8 @@ WHEEL_NAME = "wheel-requirements.tar.gz"
 SETTINGS_FORMAT = 'aert_bookkeeping_site.settings.{}'
 REQUIRE_FORMAT = 'requirements/{}.txt'
 
-def _md5_for_file(filename, block_size=2**20):
+
+def _md5_for_file(filename, block_size=2 ** 20):
     f = open(filename)
     md5 = hashlib.md5()
     while True:
@@ -31,6 +32,7 @@ def _md5_for_file(filename, block_size=2**20):
     f.close()
     return md5.hexdigest()
 
+
 @task
 def make_wheel(env='staging'):
     """ Create new wheel requirements file """
@@ -40,9 +42,8 @@ def make_wheel(env='staging'):
     with prefix('. {}/bin/activate'.format(VENV_PATH)):
         # Get all the requirements
         print colors.green("Downloading and compiling requirements. This could take several minutes...")
-        local('pip wheel --wheel-dir={wheel} -r {require_path}'\
-                               .format(wheel=WHEEL_PATH + '/' + PROJECT_NAME,
-                                       require_path=require_path))
+        local('pip wheel --wheel-dir={wheel} -r {require_path}'.format(wheel=WHEEL_PATH + '/' + PROJECT_NAME,
+                                                                       require_path=require_path))
 
     # Zip up
     print colors.green("Zipping all the requirements into one file...")
@@ -52,4 +53,5 @@ def make_wheel(env='staging'):
 
     # Create a MD5
     md5 = _md5_for_file(WHEEL_PATH + '/' + WHEEL_NAME)
-    print colors.green('Upload the requirements and set the following MD5 in your pillar configuration: {md5}'.format(md5=md5))
+    print colors.green(
+        'Upload the requirements and set the following MD5 in your pillar configuration: {md5}'.format(md5=md5))
