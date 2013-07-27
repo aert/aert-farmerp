c2d25102fbed4b84505c27c3e3b815b1e6eaa298
aert <dev.aert@gmail.com>
2013-07-27 18:09:31 +0200

    New wheel generation method

--- deployment/fabric_tasks/dev.py
+++ deployment/fabric_tasks/dev.py
@@ -1,4 +1,5 @@
-from os.path import dirname, realpath, join
+from os.path import join, basename
+from os import environ
 import hashlib
 
 from fabric.api import *
@@ -6,21 +7,20 @@ from fabric import colors
 
 
 # Local paths
-LOCAL_ROOT = dirname(dirname(realpath(__file__)))
-VENV_PATH = "/home/ari/Dev/Envs/aert-bookkeeping"
+VENV_ENV = environ['VIRTUAL_ENV']
+VENV_PROJECT = join(environ['PROJECT_HOME'], basename(VENV_ENV))
 
 
 # Server paths
 PROJECT_NAME = "aert_bookkeeping"
 
-WHEEL_PATH = join(LOCAL_ROOT, "wheel")
-WHEEL_PATH_DEPLOY = join(LOCAL_ROOT, "salt", "roots",
-                         "salt", "aert_bookkeeping")
-WHEEL_NAME = "wheel-requirements.tar.gz"
+WHEEL_TMP_PATH = join(VENV_PROJECT, "build", "wheel")
+WHEEL_DEPLOY_PATH = join(VENV_PROJECT, "build")
+WHEEL_NAME = "{}_wheel-reqs.tar.gz".format(PROJECT_NAME)
 
 # Other
 SETTINGS_FORMAT = 'aert_bookkeeping_site.settings.{}'
-REQUIRE_FORMAT = 'requirements/{}.txt'
+REQUIRE_PATH_FMT = 'deployment/requirements/{}.txt'
 
 
 def _md5_for_file(filename, block_size=2 ** 20):
@@ -38,26 +38,32 @@ def _md5_for_file(filename, block_size=2 ** 20):
 @task
 def make_wheel(env='staging'):
     """ Create new wheel requirements file """
-    require_path = join(LOCAL_ROOT,
-                        REQUIRE_FORMAT.format(env))
+    require_path = join(VENV_PROJECT,
+                        REQUIRE_PATH_FMT.format(env))
 
-    with prefix('. {}/bin/activate'.format(VENV_PATH)):
+    with prefix('. {}/bin/activate'.format(VENV_ENV)):
         # Get all the requirements
         print colors.green("Downloading and compiling requirements."
                            "This could take several minutes...")
         local('pip wheel --wheel-dir={wheel} -r {require_path}'
-              .format(wheel=WHEEL_PATH + '/' + PROJECT_NAME,
+              .format(wheel=WHEEL_TMP_PATH + '/' + PROJECT_NAME,
               require_path=require_path))
 
     # Zip up
     print colors.green("Zipping all the requirements into one file...")
-    with lcd(WHEEL_PATH):
+    with lcd(WHEEL_TMP_PATH):
         local('tar czf {name} {project}/'.format(name=WHEEL_NAME,
                                                  project=PROJECT_NAME))
-        local('mv {name} {salt_path}/{name}'
-              .format(name=WHEEL_NAME, salt_path=WHEEL_PATH_DEPLOY))
+        local('mv {name} {deploy_path}/{name}'
+              .format(name=WHEEL_NAME, deploy_path=WHEEL_DEPLOY_PATH))
+
+    # Remove working dir
+    local('rm -rf {}'.format(WHEEL_TMP_PATH))
 
     # Create a MD5
-    md5 = _md5_for_file(WHEEL_PATH_DEPLOY + '/' + WHEEL_NAME)
+    wheel_archive = join(WHEEL_DEPLOY_PATH, WHEEL_NAME)
+    md5 = _md5_for_file(wheel_archive)
+    with open(wheel_archive + ".md5", "a") as out:
+        out.write(md5)
     print colors.green("Upload the requirements and set the following MD5 in"
                        " your pillar configuration: {md5}".format(md5=md5))
