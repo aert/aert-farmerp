49007f38f2f8d99460ae93983d8212c5b6095334
aert <dev.aert@gmail.com>
2013-09-06 22:59:26 +0200

    modified: fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -3,28 +3,48 @@ from fabric import colors
 
 
 @task
+def init_env_local():
+    """ Setup development env. """
+    local('pip install -r deploy/requirements/local.txt')
+
+    with lcd('sources'):
+        local('add2virtualenv `pwd`')
+
+    local("sudo npm install -g less")
+    local("sudo npm install -g coffee-script")
+
+
+@task
+def init_env_vagrant():
+    """ Re-create & configure vagrant vm. """
+    _vagrant_init()
+    _vagrant_run_ansible()
+
+
+@task
 def run_tests():
-    """ Run test suite """
+    """ Run test suite. """
     local('py.test sources')
 
 
 @task
-def build_clean():
-    """ Delete build dir content & pyc files """
+def clean():
+    """ Delete build & public dir. """
+    local('rm -rf docs/build/*')
     local('rm -rf build/*')
+    local('rm -rf public/assets/*')
     local('pyclean .')
 
 
 @task
 def build_wheel():
-    """ Create new wheel file """
+    """ Create new wheel file. """
     with lcd('deploy/fabric'):
         local('fab build.wheel')
 
 
-@task
-def vagrant_run_ansible():
-    """ Start vagrant and run top.yml """
+def _vagrant_run_ansible():
+    """ Start vagrant and run top.yml. """
     print colors.yellow("[ Starting vagrant... ]")
     local('vagrant up')
     print colors.yellow("[ Running ansible playbooks... ]")
@@ -32,9 +52,8 @@ def vagrant_run_ansible():
         local('ansible-playbook -i hosts/vagrant top.yml -v')
 
 
-@task
-def vagrant_init():
-    """ Destroy vagrant & recreate it """
+def _vagrant_init():
+    """ Destroy vagrant & recreate it. """
     print colors.yellow("[ Initializing vagrant... ]")
     local('vagrant destroy')
     local('vagrant up')
